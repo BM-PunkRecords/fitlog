ff49ecfe1fb675ace43d6b8d0786e7abe79f7861
BeomMin Park <club20608@gmail.com>
2026-07-28 19:42:15 +0900

    fix: deny SW navigation fallback for ads.txt

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff2}'],
         maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
+        navigateFallbackDenylist: [/^\/ads\.txt$/],
       },
     }),
   ],
